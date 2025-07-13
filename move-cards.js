e1572e5cfb5037132388b7e6b2a34829eb5de999
Noah Robinson <robinson.noah@outlook.com>
2025-07-13 07:26:49 +0100

    Add files via upload

--- move-cards.js
+++ move-cards.js
@@ -1,6 +1,15 @@
 let cards = document.getElementsByClassName("card");
 
+function reassignIds() {
+  for (let i = 0; i < cards.length; i++) {
+    let cards = document.getElementsByClassName("card");
+    cards[i].id = "card" + i
+  }
+}
+
 for (let i = 0; i < cards.length; i++) {
+  reassignIds()
+  // FOR MOVING CARDS AROUND
   cards[i].addEventListener("dragstart", (event) => {
     event.dataTransfer.setData("text", event.target.id);
   });
@@ -14,5 +23,13 @@ for (let i = 0; i < cards.length; i++) {
     prevcardclass = String(prevcard.classList);
     prevcard.classList = event.target.classList;
     event.target.classList = prevcardclass;
+  });
+  // FOR SELECTING CARDS
+  cards[i].addEventListener("click", (event) => {
+    if (event.target.classList.contains("selected")) {
+      event.target.classList.remove("selected")
+    } else {
+      event.target.classList.add("selected");
+    }
   });
 }
